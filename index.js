57a09a59caaa43ad948172e4313ec454bd0a81db
DuongKien <ddk0705@gmail.com>
2018-07-15 17:19:06 +0700

    Add logic to index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 
-import { NativeModules } from 'react-native';
+'use strict';
+
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { RNPlayerMusic } = NativeModules;
+
+class RNPlayerMusic {
+
+    static play(songName) {
+        return new Promise((resolve, reject) => {
+            RNPlayerMusic.play(songName, res => {
+                resolve(res);
+            })
+        });
+    }
+
+    static stop() {
+        RNPlayerMusic.stop();
+    }
+
+}
 
 export default RNPlayerMusic;
